751012ef74030fd5fe956d11f88607d7383e1266
astutesparrow <liuzhaomin@yeah.net>
2013-04-16 22:06:23 +0800

    java concurrency in practice的例子

--- net/jcip/examples/SingleThreadWebServer.java
+++ net/jcip/examples/SingleThreadWebServer.java
@@ -11,7 +11,6 @@ import java.net.Socket;
  *
  * @author Brian Goetz and Tim Peierls
  */
-
 public class SingleThreadWebServer {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = new ServerSocket(80);
